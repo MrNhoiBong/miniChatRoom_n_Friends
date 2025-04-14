17ae82fa1dfe5d6f2a81ec5107db494a86e8adab
KIM LONG <15kimlong8@gmail.com>
2025-04-14 12:31:36 +0700

    Listen request from Client

--- src/main/java/ChatRoom_server/Interface/Login.java
+++
@@ -1,7 +0,0 @@
-package ChatRoom_server.Interface;
-
-public abstract class Login extends Thread{
-    public abstract Boolean check(String userName, String userPw);
-    @Override
-    public abstract void run();
-}
