b818a07918fba6d21cd2c5183e226c609c2df764
KIM LONG <15kimlong8@gmail.com>
2025-04-19 14:37:32 +0700

    Update Server

--- src/main/java/ChatRoom_server/Main.java
+++ src/main/java/ChatRoom_server/Main.java
@@ -7,22 +7,27 @@ import ChatRoom_server.Interface.DataBase;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Main {
     public static volatile ServerSocket server;
     public static void main(String[] args) {
         DataBase dataBase = new txtDataBase();
         dataBase.Load();
+
+        ASCIIart();
+
         Thread inputThread = new Thread(() -> {
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-                System.out.println("Nhập 'exit' để thoát:");
+                System.out.println("Type exit for stop server");
                 while (true) {
                     String input = reader.readLine(); // Đọc dòng từ bàn phím
                     if ("exit".equalsIgnoreCase(input)) {
                         server.close(); // Cập nhật giá trị biến stop
-                        System.out.println("Đã thoát!");
+                        System.out.println("Saved data and stop server");
                         break;
                     }
                 }
@@ -43,6 +48,26 @@ public class Main {
         } catch (IOException e) {
             dataBase.Save();
             System.out.println("exception");
+            System.exit(1);
+        }
+    }
+
+    public static void ASCIIart(){
+        System.out.println("███╗   ███╗██╗███╗   ██╗██╗ ██████╗██╗  ██╗ █████╗ ████████╗██████╗  ██████╗  ██████╗ ███╗   ███╗");
+        System.out.println("████╗ ████║██║████╗  ██║██║██╔════╝██║  ██║██╔══██╗╚══██╔══╝██╔══██╗██╔═══██╗██╔═══██╗████╗ ████║");
+        System.out.println("██╔████╔██║██║██╔██╗ ██║██║██║     ███████║███████║   ██║   ██████╔╝██║   ██║██║   ██║██╔████╔██║");
+        System.out.println("██║╚██╔╝██║██║██║╚██╗██║██║██║     ██╔══██║██╔══██║   ██║   ██╔══██╗██║   ██║██║   ██║██║╚██╔╝██║");
+        System.out.println("██║ ╚═╝ ██║██║██║ ╚████║██║╚██████╗██║  ██║██║  ██║   ██║   ██║  ██║╚██████╔╝╚██████╔╝██║ ╚═╝ ██║");
+        System.out.println("╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝     ╚═╝");
+        try {
+            // Lấy địa chỉ IP của máy
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            // In ra địa chỉ IP
+            System.out.println("IP : " + inetAddress.getHostAddress());
+            System.out.println("Port: 9911");
+        } catch (UnknownHostException e) {
+            // Xử lý ngoại lệ nếu không thể lấy địa chỉ IP
+            System.out.println("Không thể lấy địa chỉ IP.");
             System.exit(1);
         }
     }
