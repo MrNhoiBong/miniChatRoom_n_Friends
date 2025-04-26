9184749d5ba1661ed713239903a9ba3f463db62c
KIM LONG <15kimlong8@gmail.com>
2025-04-26 09:28:54 +0700

    update gitignore

--- src/main/java/ChatRoom_Console_client/Main.java
+++ src/main/java/ChatRoom_Console_client/Main.java
@@ -2,6 +2,6 @@ package ChatRoom_Console_client;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("usout sdlkas");
+        System.out.println("testing another file");
     }
 }
